06a45b128f465c1192db8b231ff4bd2f057fbc92
isaaacp <pearl.isaac@gmail.com>
2019-03-11 14:29:53 -0400

    nav bar components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import PropTypes from 'prop-types';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function ErrorBox({ children }) {
+	return(
+		<div className="error-box">
+			<span className="alert-icon"><i className="fas fa-exclamation-triangle"/></span>
+			<span className="text">{children}</span>
+		</div>
+	);
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Nav ({ children }) {
+	return(
+		<div className="nav-bar">
+			{children.map((child, i) => <div key={i}> {child} </div>)}
+		</div>
+	);
+}
+/*
+the below function is from https://stackoverflow.com/questions/27366077/only-allow-children-of-a-specific-type-in-a-react-component, 
+understand this better before moving on!
+*/
+Nav.propTypes = {
+	children: function (props, propName, componentName) {
+	    const prop = props[propName]
+
+	    var error = null;
+	    React.Children.forEach(prop, function (child) {
+	      if (child.type !== NavItem) {
+	        error = new Error('`' + componentName + '` children should be of type `NavItem`.');
+	      }
+	    })
+	    return error
+  }
+}
+
+function NavItem({ url, children }) {
+	return (	
+		<a className="nav-item" href={url}>{children}</a>
+	);
+}
+
+ReactDOM.render(
+	<div>
+		<ErrorBox>
+			The world is ending
+		</ErrorBox>
+		<Nav>
+			<NavItem url='/'>Home</NavItem>
+			<NavItem url='/about'>About</NavItem>
+			<NavItem url='/contact'>Contact</NavItem>
+		</Nav>
+	</div>,
+	document.querySelector('#root'));
